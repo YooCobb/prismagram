ce2a961a7d4c6d8dfa56e03c9f734c2c2f8e6c82
YooCobb <yhj010597@naver.com>
2020-01-14 22:47:20 +0900

    start~3-10

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-mailgun-transport";
+import jwt from "jsonwebtoken";
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_key: process.env.MAILGUN_API,
+      domain: process.env.MAILGUN_DOMAIN
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "nico@prismagram.com",
+    to: adress,
+    subject: "Login Secret for Prismagram",
+    html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`
+  };
+  return sendMail(email);
+};
+
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
